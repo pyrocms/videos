4946c4399d2e9fefdbceb389a23b1e3859ddfa91
Jaap Jolman <jaap@jolman.eu>
2012-02-04 21:17:42 +0100

    Dutch Language Added

---
+++ language/dutch/video_lang.php
@@ -0,0 +1,48 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// labels
+$lang['video:video_label']             	      = 'Video';
+$lang['video:channel_label']                  = 'Kanaal';
+$lang['video:live_label']                  	  = 'Live';
+$lang['video:view_label']                     = 'Bekijken';
+$lang['video:preview_label']                  = 'Voorbeeld';
+$lang['video:feature_label']                  = 'Feature';
+$lang['video:date_label']                     = 'Datum';
+$lang['video:time_label']                     = 'Tijd';
+$lang['video:details_label']                  = 'Details';
+$lang['video:options_label']                  = 'Opties';
+$lang['video:title_label']                    = 'Titel';
+$lang['video:slug_label']                     = 'URL';
+$lang['video:thumbnail_label']                = 'Thumbnail';
+$lang['video:embed_code_label']               = 'Embed Code';
+$lang['video:intro_label']             		  = 'Intro';
+$lang['video:width_label']        		      = 'Breedte';
+$lang['video:height_label']        		      = 'Hoogte';
+$lang['video:access_label']        		      = 'Toegang';
+$lang['video:views_label']        		      = 'Views';
+$lang['video:new_channel_label']              = 'een kanaal toevoegen';
+$lang['video:comments_enabled_label']         = 'Commentaar Ingeschakelt';
+$lang['video:schedule_on_label']              = 'Geplanded';
+
+// titles
+$lang['video:create_title']                   = 'Video Toevoegen';
+$lang['video:edit_title']                     = 'Berwerk Video "%s"';
+$lang['video:video_title']                    = 'Video';
+$lang['video:videos_title']                   = 'Video\'s';
+
+$lang['video:related_videos']                 = 'Gerelateerde videos';
+$lang['video:channel_videos']                 = 'Andere videos in %s';
+$lang['video:more_videos']                    = 'Meer Bekijken';
+
+// messages
+$lang['video:no_videos']                      = 'Er zijn geen video\'s.';
+$lang['video:subscripe_to_rss_desc']          = 'Krijg video\'s direct door u te abonneren op onze RSS feed. U kunt dit doen via de meeste populaire e-mailclients, of probeer <a href="http://reader.google.co.uk/">Google Reader</a>.';
+$lang['video:currently_no_videos']            = 'Er zijn geen video\'s op het moment.';
+$lang['video:video_add_success']              = 'De video "%s" is toegevoegd.';
+$lang['video:already_exist_error']			  = 'Een video met het slug "%s" bestaad al.';
+$lang['video:video_add_error']                = 'Er is een fout opgetreden.';
+$lang['video:edit_success']                   = 'De video "%s" is bijgewerkt.';
+$lang['video:edit_error']                     = 'Er is een fout opgetreden.';
+$lang['video:delete_success']                 = 'De Video "%s" is verwijderd.';
+$lang['video:mass_delete_success']            = 'De video\'s "%s" zijn verwijderd.';
+$lang['video:delete_error']                   = 'Er zijn geen video\'s verwijderd.';
